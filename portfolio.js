d441e49147bff764285c0159736c53cbbf4c38b7
Home <elias.norstromm@gmail.com>
2018-06-17 23:32:25 +0100

    adds nav-toggle icon

--- portfolio.js
+++ portfolio.js
@@ -5,14 +5,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 document.querySelector('.nav-toggle-button').addEventListener('click', toggleNavButtons);
 
+let navButtonIcon = document.getElementById("nav-icon");
 let navButtons = document.querySelectorAll('.nav-container > li');
 let maxWidth = matchMedia("(max-width: 700px)");
 let hiddenItems = true;
 let show = 'inherit';
 let hide = 'none';
+let cross = 'fa-times';
+let orgiginalIcon = 'fa-bars';
 
 maxWidth.addListener(showNavOnWide);
 
+// Helper function to toggle the navbar icon on smaller screens
+let toggleIcon = function(element, icon) {
+  element.classList.toggle(icon);
+  if (!maxWidth.matches) {
+    element.classList.remove(icon);
+  }
+};
 // Helper function to set the display property of each child of a parent
 let showOrHide = (parent, state) => {
   parent.forEach((element) => {
@@ -21,37 +31,23 @@ let showOrHide = (parent, state) => {
 }
 
 function showNavOnWide() {
-  maxWidth.matches ? showOrHide(navButtons, hide) : showOrHide(navButtons, show);
+  hiddenItems = true;
+  if (maxWidth.matches) {
+    showOrHide(navButtons, hide);
+  } else {
+    showOrHide(navButtons, show);
+    toggleIcon(navButtonIcon, cross);
+  }
 };
 
 function toggleNavButtons() {
   if (hiddenItems) {
     showOrHide(navButtons, show);
+    toggleIcon(navButtonIcon, cross);
     hiddenItems = false;
   } else {
       showOrHide(navButtons, hide);
+      toggleIcon(navButtonIcon, cross);
       hiddenItems = true;
   }
 }
-
-
-
-
-
-
-
-
-
-// function toggleNavButtons() {
-//   if (hiddenItems) {
-//     navButtons.forEach((thing) => {
-//       thing.style.display = 'inherit';
-//       hiddenItems = false;
-//     });
-//   } else {
-//       navButtons.forEach((thing) => {
-//         thing.style.display = 'none';
-//         hiddenItems = true;
-//     });
-//   }
-// }
